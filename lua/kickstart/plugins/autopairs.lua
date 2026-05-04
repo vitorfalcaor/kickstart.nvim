d6c02222e649de628e7d9339bc7d71bfb589f0e3
Vitor <vitor.falcaor@gmail.com>
2026-05-03 22:09:15 -0300

    zig: adding semi-colon on struct opening

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -5,4 +5,15 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
+  config = function(_, opts)
+    local npairs = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
+
+    npairs.setup(opts)
+
+    -- Zig struct declarations end with a semicolon: const Foo = struct { ... };
+    npairs.add_rule(Rule('.*struct%s+{$', '};', 'zig'):use_regex(true, '{'))
+    npairs.add_rule(Rule('.*return%s+%.%s*{$', '};', 'zig'):use_regex(true, '{'))
+    npairs.add_rule(Rule('.*return%s+[%w_%.]+%s*{$', '};', 'zig'):use_regex(true, '{'))
+  end,
 }
